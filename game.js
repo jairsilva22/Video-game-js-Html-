14bdb79e1a139267a532bd5f37788f8dd22616a9
Jair Silva <luis.silor@gmail.com>
2023-07-25 15:58:52 -0600

    dandole tamaño al canvas

--- game.js
+++ game.js
@@ -1,4 +1,31 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-game.fill
+window.addEventListener('load',startGame);
+
+
+function startGame(){
+  
+let canvasSize;
+
+if(window.innerHeight>window.innerWidth){
+    canvasSize= window.innerWidth * .80;
+}else{
+    canvasSize = window.innerHeight * .80;
+}
+
+ canvas.setAttribute('Width',canvasSize);
+ 
+ canvas.setAttribute('Height',canvasSize);
+
+ const elementSize = canvasSize /10;
+
+ game.font= elementSize +'px Verdana';
+ game.position='end';
+ game.fillText('💣',elementSize,elementSize);
+
+
+
+}
+
+
